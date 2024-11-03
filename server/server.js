619da2b40cfc47924e69f9c5c33593fff4f71ed2
Matheus <matthewcsilva@gmail.com>
2024-11-03 17:18:32 -0500

    Fixing puppeteer

--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ const obterResultadosLotofacil = async () => {
 });
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36');
+  await page.setViewport({ width: 1280, height: 800 });
   
   await page.goto(LOTOFACIL_URL, { waitUntil: 'networkidle2' });
 
