9da92cebda701cb7eb34d2e3ec0e39d373d3571e
Matheus <matthewcsilva@gmail.com>
2024-11-03 23:17:28 -0500

    Fixing

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const LOTOFACIL_URL = 'https://loterias.caixa.gov.br/Paginas/Lotofacil.aspx';
 // Função para obter os resultados usando Puppeteer
 const obterResultadosLotofacil = async () => {
   const browser = await puppeteer.launch({ 
-	headless: 'new',
+	headless: true,
 	args: [
 		'--no-sandbox',
 		'--disable-setuid-sandbox',
