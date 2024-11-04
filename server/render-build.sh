7b07b8a5f721d2f09d8e4b276ae67ee089512014
Matheus <matthewcsilva@gmail.com>
2024-11-03 22:37:22 -0500

    Fixing

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
-# exit on errorset -o errexit
+set -o errexit  # Exit on error
+
+# Set default values if environment variables are not set
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-/root/.cache}
+PUPPETEER_CACHE_DIR=${PUPPETEER_CACHE_DIR:-/opt/render/project/puppeteer}
 
 npm install
-# npm run build # uncomment if required
+# npm run build  # Uncomment if you need to build the project
+
+# Ensure the cache directories exist
+mkdir -p $XDG_CACHE_HOME/puppeteer
+mkdir -p $PUPPETEER_CACHE_DIR
 
 # Store/pull Puppeteer cache with build cache
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
